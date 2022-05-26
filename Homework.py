3dc54343adada8bb05f1d46aa8962a2bc112360f
Cory Shock <91140740+cshock159@users.noreply.github.com>
2022-05-26 00:26:30 -0700

    Update Homework.py

--- Homework.py
+++ Homework.py
@@ -1,53 +1,53 @@
-import string
-import random
-import sys
-
+import string, random, sys
 from time import sleep
+
+def main():
+    setup()
+    print("Your random character string is: " + ''.join(randomstring))
+    sleep(5)
+    while 1 == 1:
+        currentchar = 0
+        nextchar = currentchar + 1
+        priorchar = currentchar - 1
+        runthrough(priorchar, currentchar, nextchar)
 
 def setup():
     global allones
     global allzeros
     global randomstring
     global length
+    global allones
+    global allzeros
+    global replacement
     length = int(sys.argv[1])
     randomstring = ''.join([random.choice(string.digits[:2]) for item in range(length)])
     allzeros = ''.join([random.choice(string.digits[0]) for item in range(length)])
     allones = ''.join([random.choice(string.digits[1]) for item in range(length)])
     randomstring = list(randomstring)
-    return randomstring, allzeros, allones
+    return randomstring
 
 def runthrough(priorchar, currentchar, nextchar):
     while currentchar <= length:
-        nonstopticker = 0
-        # when restarting the search through string
-        if currentchar == 0:
-            currentThree = randomstring[-1] + randomstring[currentchar] + randomstring[nextchar]
-            returns(currentThree, currentchar, randomstring)
-        #Where NEXTCHAR goes into out of index. In 5, here prior is 3, current is 4, and next is 5(Out of index)
+        # This resets the nextchar value back to zero, so it will pull the first substring character(digit) in randomstring, completing the loop around.
         if priorchar == (length - 2):
             nextchar = 0
             currentThree = randomstring[priorchar] + randomstring[currentchar] + randomstring[nextchar]
             returns(currentThree, currentchar, randomstring)  
-        #Max Limit (I.E. 5 - 1 = 4 is the max index)        
+        # Serves the same purpose as above, but for current character, as well as prior character, after it gets used one last time.       
         if priorchar == (length - 1):
             currentchar = 0
             currentThree = randomstring[priorchar] + randomstring[0] + randomstring[nextchar]
             returns(currentThree, currentchar, randomstring)
             priorchar = -1
-        # Generic Catch all.
+        # Everyone else will land here 
         elif currentchar > 0 and currentchar <= (length - 1) and nextchar > 0:
             currentThree = randomstring[priorchar] + randomstring[currentchar] + randomstring[nextchar]
             returns(currentThree, currentchar, randomstring)
         currentchar += 1
         nextchar += 1
         priorchar += 1
-        nonstopticker += 1
-        if nonstopticker == 1000:
-            print("It looks like you've been waiting a while. This may go on to infinity.")
-            sleep(10)
 
 def returns(currentThree, currentchar, randomstring):
-    global replacement
     match currentThree:
         case '111':
             replacement = '0'
@@ -65,7 +65,7 @@ def returns(currentThree, currentchar, randomstring):
             replacement = '1'
         case '000':
             replacement = '0'
-
+            
     randomstring[currentchar] = replacement
     randomstring = ''.join(randomstring)
     check(randomstring)
@@ -76,20 +76,9 @@ def check(randomstring):
         print(randomstring + " has complete uniformity!")
         exit()
     else:
-        print("Not quite uniform yet. Going around with the new number " + randomstring)
+        print("Not quite uniform yet. Going around with the number " + randomstring)
         randomstring = list(randomstring)
         return randomstring
-
-def main():
-    setup()
-    print("Your random character string is: " + ''.join(randomstring))
-    sleep(5)
-    while 1 == 1:
-        currentchar = 0
-        nextchar = currentchar + 1
-        priorchar = currentchar - 1
-        runthrough(priorchar, currentchar, nextchar)
-
 
 if __name__ == "__main__":
     main()
