2de95fc67ac9d4f3fb29f3d4dc796baaabc67de0
Cory Shock <91140740+cshock159@users.noreply.github.com>
2022-05-25 23:39:52 -0700

    Update Homework.py

--- Homework.py
+++ Homework.py
@@ -1,57 +1,54 @@
-from asyncio.windows_events import NULL
-from asyncore import ExitNow
 import string
 import random
-import time
 import sys
 
 from time import sleep
 
-sys.setrecursionlimit(50000)
-length = int(input("How many numbers deep do you want to go? "))
-randomstring = ''.join([random.choice(string.digits[:2]) for item in range(length)])
-allzeros = ''.join([random.choice(string.digits[0]) for item in range(length)])
-allones = ''.join([random.choice(string.digits[1]) for item in range(length)])
-randomstring = list(randomstring)
+def setup():
+    global allones
+    global allzeros
+    global randomstring
+    global length
+    #length = int(sys.argv[1])
+    length = int(input("How many numbers deep do you want to go? "))
+    randomstring = ''.join([random.choice(string.digits[:2]) for item in range(length)])
+    allzeros = ''.join([random.choice(string.digits[0]) for item in range(length)])
+    allones = ''.join([random.choice(string.digits[1]) for item in range(length)])
+    randomstring = list(randomstring)
+    return randomstring, allzeros, allones
 
-currentchar = 0
-nextchar = currentchar + 1
-priorchar = currentchar - 1
-
-def runThrough():
-    global currentchar
-    global nextchar
-    global priorchar
+def runthrough(priorchar, currentchar, nextchar):
     while currentchar <= length:
+        nonstopticker = 0
         # when restarting the search through string
         if currentchar == 0:
-            print(str(priorchar) + str(currentchar) + str(nextchar) + " in third Slot")
             currentThree = randomstring[-1] + randomstring[currentchar] + randomstring[nextchar]
             returns(currentThree, currentchar, randomstring)
         #Where NEXTCHAR goes into out of index. In 5, here prior is 3, current is 4, and next is 5(Out of index)
         if priorchar == (length - 2):
             nextchar = 0
-            print(str(priorchar) + str(currentchar) + str(nextchar) + " in First Slot") 
             currentThree = randomstring[priorchar] + randomstring[currentchar] + randomstring[nextchar]
             returns(currentThree, currentchar, randomstring)  
         #Max Limit (I.E. 5 - 1 = 4 is the max index)        
         if priorchar == (length - 1):
             currentchar = 0
-            print(str(priorchar) + str(currentchar) + str(nextchar) + " in second Slot")
             currentThree = randomstring[priorchar] + randomstring[0] + randomstring[nextchar]
             returns(currentThree, currentchar, randomstring)
             priorchar = -1
-        elif currentchar > 0 and currentchar <= 4 and nextchar > 0:
-            print(str(priorchar) + str(currentchar) + str(nextchar) + "in last Slot")
+        # Generic Catch all.
+        elif currentchar > 0 and currentchar <= (length - 1) and nextchar > 0:
             currentThree = randomstring[priorchar] + randomstring[currentchar] + randomstring[nextchar]
             returns(currentThree, currentchar, randomstring)
         currentchar += 1
         nextchar += 1
         priorchar += 1
+        nonstopticker += 1
+        if nonstopticker == 1000:
+            print("It looks like you've been waiting a while. This may go on to infinity.")
+            sleep(10)
 
 def returns(currentThree, currentchar, randomstring):
     global replacement
-    replacement = ''
     match currentThree:
         case '111':
             replacement = '0'
@@ -75,17 +72,25 @@ def returns(currentThree, currentchar, randomstring):
     check(randomstring)
 
 def check(randomstring):
-    print("checking " + randomstring)
+    print("Checking to see if " + randomstring + " is uniform.")
     if randomstring == allones or randomstring == allzeros:
-        print(randomstring + " Matches the all ones or zeros!")
+        print(randomstring + " has complete uniformity!")
         exit()
     else:
-        print("not quite done yet. Another round with " + randomstring)
+        print("Not quite uniform yet. Going around with the new number " + randomstring)
         randomstring = list(randomstring)
         return randomstring
 
+def main():
+    setup()
+    print("Your random character string is: " + ''.join(randomstring))
+    sleep(5)
+    while 1 == 1:
+        currentchar = 0
+        nextchar = currentchar + 1
+        priorchar = currentchar - 1
+        runthrough(priorchar, currentchar, nextchar)
 
-runThrough()
 
-
-            
+if __name__ == "__main__":
+    main()
